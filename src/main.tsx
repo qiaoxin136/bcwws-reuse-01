152b67946fa2b83e7576e5cb7994c33729d72892
Xin Qiao <qiaoxin136@gmail.com>
2026-05-17 07:32:39 -0400

    add logo

--- src/main.tsx
+++ src/main.tsx
@@ -7,15 +7,25 @@ import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 import '@aws-amplify/ui-react/styles.css';
 
-
-
-
 Amplify.configure(outputs);
 
+const components = {
+  Header() {
+    return (
+      <div style={{ display: 'flex', justifyContent: 'center', padding: '32px 0 32px' }}>
+        <img
+          src="/logo.png"
+          alt="Thompson & Associates"
+          style={{ maxWidth: '480px', width: '100%' }}
+        />
+      </div>
+    );
+  },
+};
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    
   <React.StrictMode>
-    <Authenticator>
+    <Authenticator components={components}>
       <App />
     </Authenticator>
   </React.StrictMode>
